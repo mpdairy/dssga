9ea2e6a3b3ca3c480ebfaf5e558fe2c5b521c1ca
Matt Parker <mpdairy@gmail.com>
2014-10-03 17:55:10 -0400

    The initial commit.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dssga
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/dssga/core_test.clj
@@ -0,0 +1,7 @@
+(ns dssga.core-test
+  (:require [clojure.test :refer :all]
+            [dssga.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
